8bb13ad0890577d6de684e157bfa14d11ecdfd75
Daniel <alteonrene@gmail.com>
2024-01-30 18:29:26 -0500

    Transaction Object

---
+++ Transaction.py
@@ -0,0 +1,21 @@
+import uuid
+import time
+
+
+# Transaction class with attributes that uniquely details a transaction on the block. The transaction object is
+# a funciton of a block. One of the core aspects of Blockchain.
+class Transaction():
+
+    def __init__(self, senderPublicKey, receiverPublicKey, amount, type):
+        self.senderPublicKey = senderPublicKey
+        self.receiverPublicKey = receiverPublicKey
+        self.amount = amount
+        self.type = type
+        self.id = uuid.uuid1().hex
+        self.timestamp = time.time()
+        self.signature = ''
+
+# Method that returns attributes of Transaction object in a dictionary
+    def toJSON(self):
+        return self.__dict__
+
