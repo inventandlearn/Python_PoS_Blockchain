82ecbdb69c9acf6b50cba44c0161e1e1c8e5da18
Daniel <alteonrene@gmail.com>
2024-02-15 11:02:19 -0500

    Added method that resets signature value of each block added to the chain

--- Block.py
+++ Block.py
@@ -1,4 +1,5 @@
 import time
+import copy
 
 # The Block object is used to extract transactions from the Transaction Pool object, contain them, and subsequently link to a
 # Block previously created on the blockchain or become the genesis block within the blockchain.
@@ -25,3 +26,12 @@ class Block():
             jsonTransactions.append(transaction.toJSON())
         data['transactions'] = jsonTransactions
         return data
+
+
+# Method that keeps signature consistent for each Block. In other words the signature of the last Block doesn't carry over to the following Block.
+# After each Block object is added to the chain with a unique signature the next Block instantiated has it's signature reset to an empty string.
+# Once the block has been successfully added to the chain, the signature created will fill the empty string.
+    def payload(self):
+        jsonRepresentation = copy.deepcopy(self.toJSON)
+        jsonRepresentation['signature'] = ''
+        return jsonRepresentation
