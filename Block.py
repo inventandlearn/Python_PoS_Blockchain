d09f7cfa1a4f1b719fd5527b0829bc2249775f55
Daniel <alteonrene@gmail.com>
2024-02-15 10:47:22 -0500

    Added comments to describe Block object and methods

--- Block.py
+++ Block.py
@@ -1,5 +1,7 @@
 import time
 
+# The Block object is used to extract transactions from the Transaction Pool object, contain them, and subsequently link to a
+# Block previously created on the blockchain or become the genesis block within the blockchain.
 class Block():
 
     def __init__(self, transactions, lastHash, forger, blockCount):
@@ -10,6 +12,7 @@ class Block():
         self.timestamp = time.time()
         self.signature = ''
 
+# This method returns attributes of the Block object in a dictionary format.
     def toJSON(self):
         data = {}
         data['lastHash'] = self.lastHash
