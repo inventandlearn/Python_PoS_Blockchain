42d829e0ee431deef85fae4a373a2eff8ca2eda6
Daniel <alteonrene@gmail.com>
2024-02-21 07:57:28 -0500

    Added method that returns a dictionary of all blocks added to Blockchain object

--- Blockchain.py
+++ Blockchain.py
@@ -7,3 +7,12 @@ class Blockchain():
 # This method adds each block created to a linked list of previously created blocks.
     def addBlock(self, block):
         self.blocks.append(block)
+
+# This method returns a dictionary of all the blocks that have been added to the Blockchain object.
+    def toJSON(self):
+        data = {}
+        jsonBlocks = []
+        for block in self.blocks:
+            jsonBlocks.append(block.toJSON())
+            data['blocks'] = jsonBlocks
+            return data
