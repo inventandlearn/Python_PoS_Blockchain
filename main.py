94b2053111f88d4e22c8054e0ad53bf63f831e78
Daniel <alteonrene@gmail.com>
2024-02-25 12:03:16 -0500

    Script that demonstrates two types of transactions and how they are added to the blockchain, with the balances of each wallet up to date per transaction

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import pprint
 from BlockchainUtils import BlockchainUtils
 from AccountModel import AccountModel
 
-# Script which demonstrates covered transactions vs transactions that aren't covered with the Account Model object and blockchain methods"
+# Script which demonstrates the addition of two blocks to the blockchain with each having different types of transactions contained in them"
 if __name__ == '__main__':
 
     blockchain = Blockchain()
@@ -15,10 +15,27 @@ if __name__ == '__main__':
 
     alice = Wallet()
     bob = Wallet()
+    exchange = Wallet()
+    forger = Wallet()
+
+
+    exchangeTransaction = exchange.createTransaction(alice.publicKeyString(), 10, 'EXCHANGE')
+    if not pool.transactionExists(exchangeTransaction):
+        pool.addTransaction(exchangeTransaction)
+    coveredTransaction = blockchain.getCoveredTransactionSet(pool.transactions)
+    lastHash = BlockchainUtils.hash(blockchain.blocks[-1].payload()).hexdigest()
+    blockCount = blockchain.blocks[-1].blockCount + 1
+    blockOne = Block(coveredTransaction, forger.publicKeyString(), lastHash, blockCount)
+    blockchain.addBlock(blockOne)
+
 
     transaction = alice.createTransaction(bob.publicKeyString(), 5, 'TRANSFER')
     if not pool.transactionExists(transaction):
         pool.addTransaction(transaction)
-
     coveredTransaction = blockchain.getCoveredTransactionSet(pool.transactions)
-    print(coveredTransaction)
+    lastHash = BlockchainUtils.hash(blockchain.blocks[-1].payload()).hexdigest()
+    blockCount = blockchain.blocks[-1].blockCount + 1
+    blockTwo = Block(coveredTransaction, forger.publicKeyString(), lastHash, blockCount)
+    blockchain.addBlock(blockTwo)
+
+    pprint.pprint(blockchain.toJSON())
