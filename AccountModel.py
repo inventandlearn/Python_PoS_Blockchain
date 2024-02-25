eecbabf7615a527740929813584065a9e4899e72
Daniel <alteonrene@gmail.com>
2024-02-25 08:49:45 -0500

    Added updateBalance method to AccountModel

--- AccountModel.py
+++ AccountModel.py
@@ -1,4 +1,3 @@
-
 # The Account Model object stores the state of each wallet's balance and it's corresponding publicKeyString that is on the Blockchain.
 class AccountModel():
 
@@ -12,8 +11,14 @@ class AccountModel():
             self.accounts.append(publicKeyString)
             self.balances[publicKeyString] = 0
 
-# This method returns the balance of a wallet and it's corresponding publicKeyString
+# This method returns the balance of a wallet and it's corresponding publicKeyString.
     def getBalance(self, publicKeyString):
         if publicKeyString not in self.accounts:
             self.addAccount(publicKeyString)
         return self.balances(publicKeyString)
+
+# This method updates the balance of a given wallet and it's corresponding publicKeyString after it has transacted on the blockchain.
+    def updateBalance(self, publicKeyString, amount):
+        if publicKeyString not in self.accounts:
+            self.addAccount(publicKeyString)
+        self.balances[publicKeyString] += amount
