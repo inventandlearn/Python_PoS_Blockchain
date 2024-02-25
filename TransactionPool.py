493c41ef3358b7b3d136202fe3bb330b0ce43d8e
Daniel <alteonrene@gmail.com>
2024-02-25 12:28:45 -0500

    Added method that ensures TransactionPool is refreshed after each block is created

--- TransactionPool.py
+++ TransactionPool.py
@@ -5,11 +5,26 @@ class TransactionPool():
     def __init__(self):
         self.transactions = []
 
+# This method adds a validated transaction to a list of transactions to form the transaction pool.
     def addTransaction(self, transaction):
         self.transactions.append(transaction)
 
+# This method ensures that a transaction isn't added to the pool twice by checking for it's existence already.
     def transactionExists(self, transaction):
         for poolTransaction in self.transactions:
             if poolTransaction.equals(transaction):
                 return True
         return False
+
+# This method removes transactions that have already been added to a block, so that a fresh list of transactions can be added to the pool.
+    def removeFromPool(self, transactions):
+        newPoolTransactions = []
+        for poolTransaction in self.transactions:
+            insert = True
+            for transaction in transactions:
+                if poolTransaction.equals(transaction):
+                    insert = False
+            if insert == True:
+                newPoolTransactions.append(poolTransaction)
+        self.transactions = newPoolTransactions
+
