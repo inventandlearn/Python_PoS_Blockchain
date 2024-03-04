1bcd8ddcc0c2d91ffd59cb00284d29f4e1773726
Daniel <alteonrene@gmail.com>
2024-03-04 14:38:40 -0500

    Added send_to_node method which verifies communication between nodes via socketcommunication with messages for inbound and outbound connections

--- SocketCommunication.py
+++ SocketCommunication.py
@@ -13,10 +13,16 @@ class SocketCommunication(Node):
 # This method is a callback method that is used when a new inbound connection is made with another node that exists on the blockchain network.
     def inbound_node_connected(self, connected_node):
         print("inbound connection")
+        self.send_to_node(connected_node, 'Your node has connected successfully to this node')
 
 # This method is a callback method that is used when a new outbound connection is made with another node that exists on the blockchain network.
     def outbound_node_connected(self, connected_node):
         print("outbound connection")
+        self.send_to_node(connected_node, 'This node is the initializer of communication between the connected nodes')
+
+# This method sends messages between nodes that have established a connection with each other.
+    def node_message(self, connected_node, message):
+        print(message)
 
 
 
