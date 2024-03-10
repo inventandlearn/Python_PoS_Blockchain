d56de27af4e920cbd61228259e6de5b75aa8a1c9
Daniel <alteonrene@gmail.com>
2024-03-10 19:21:57 -0400

    Added send and broadcast methods so that all nodes within network whether directly connected or not can still send messages and broadcast changes through interconnectivity

--- SocketCommunication.py
+++ SocketCommunication.py
@@ -1,5 +1,6 @@
 from p2pnetwork.node import Node
 from PeerDiscoveryHandler import PeerDiscoveryHandler
+from SocketConnector import SocketConnector
 
 # The SocketCommunication object is utilized by different nodes within the blockchain network to discover and connect to each other for transactions, and data exchange.
 class SocketCommunication(Node):
@@ -8,25 +9,41 @@ class SocketCommunication(Node):
         super(SocketCommunication, self).__init__(ip, port, None)
         self.peers = []
         self.peerDiscoveryHandler = PeerDiscoveryHandler(self)
+        self.socketConnector = SocketConnector(ip, port)
+
+# This method allows newly created nodes created to establish a connection with the very first node instantiated
+# on the blockchain network. The newly created nodes can thereafter participate in the blockchain network's activities
+    def connectToFirstNode(self):
+        if self.socketConnector.port != 10001:
+            self.connect_with_node('localhost', 10001)
 
 # This method is used for the initiation of socket communication between nodes. This method is inherited from the Node class.
     def startSocketCommunication(self):
         self.start()
         self.peerDiscoveryHandler.start()
+        self.connectToFirstNode()
 
 # This method is a callback method that is used when a new inbound connection is made with another node that exists on the blockchain network.
     def inbound_node_connected(self, connected_node):
-        print("inbound connection")
-        self.send_to_node(connected_node, 'Your node has connected successfully to this node')
+        self.peerDiscoveryHandler.handshake(connected_node)
 
 # This method is a callback method that is used when a new outbound connection is made with another node that exists on the blockchain network.
     def outbound_node_connected(self, connected_node):
-        print("outbound connection")
-        self.send_to_node(connected_node, 'This node is the initializer of communication between the connected nodes')
+        self.peerDiscoveryHandler.handshake(connected_node)
 
 # This method sends messages between nodes that have established a connection with each other.
     def node_message(self, connected_node, message):
         print(message)
 
+# This method allows for the sending of messages to specific nodes that aren't directly connected.
+# The differentiation between this method and the node_message method is the directional flow of
+# messages between all nodes present on the network. So a message sent between two connected nodes or a message sent
+# by two or more unconnected nodes that however at still peers.
+    def send(self, receiver, message):
+        self.send_to_node(receiver, message)
+
+# This method broadcast allows for the broadcasting of messages to all nodes within the network.
+    def broadcast(self, message):
+        self.send_to_nodes(message)
 
 
