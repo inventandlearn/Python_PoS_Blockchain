84683eda086092aebd28083278de51abff5fde9b
Daniel <alteonrene@gmail.com>
2024-03-10 18:18:19 -0400

    Added PeerDiscoveryHandler module so that the SocketCommunication object can access it's methods of discovery and status updates of other nodes on the blockchain network

--- SocketCommunication.py
+++ SocketCommunication.py
@@ -1,14 +1,18 @@
 from p2pnetwork.node import Node
+from PeerDiscoveryHandler import PeerDiscoveryHandler
 
 # The SocketCommunication object is utilized by different nodes within the blockchain network to discover and connect to each other for transactions, and data exchange.
 class SocketCommunication(Node):
 
     def __init__(self, ip, port):
         super(SocketCommunication, self).__init__(ip, port, None)
+        self.peers = []
+        self.peerDiscoveryHandler = PeerDiscoveryHandler(self)
 
 # This method is used for the initiation of socket communication between nodes. This method is inherited from the Node class.
     def startSocketCommunication(self):
         self.start()
+        self.peerDiscoveryHandler.start()
 
 # This method is a callback method that is used when a new inbound connection is made with another node that exists on the blockchain network.
     def inbound_node_connected(self, connected_node):
